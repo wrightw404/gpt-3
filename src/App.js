c35a0f9c712df7b44ecc85d826d26c257030f6ec
Will <wrightw404@gmail.com>
2023-06-05 19:55:20 -0400

    created folder structure

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Brand, CTA, Navbar } from './components';
+import { Blog, Footer, Header, Features, WhatGPT3, Possibility } from './containers';
 
 const App = () => {
   return (
---
+++ src/containers/whatGPT3/WhatGTP3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './whatGPT3.css'
+
+const WhatGTP3 = () => {
+  return (
+    <div>WhatGTP3</div>
+  )
+}
+
+export default WhatGTP3
